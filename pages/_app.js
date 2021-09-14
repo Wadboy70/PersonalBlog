1580cf9289a098ceb46961ada70047961219a69b
Seyi Oluwaleimu <s.oluwaleimu@gmail.com>
2021-09-13 22:32:25 -0400

    configured everything for styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import 'components/app.scss';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Montserrat:wght@300&display=swap" rel="stylesheet" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,19 @@
 import { getPosts } from "../lib/functions";
 import Link from "next/link";
+import styles from 'components/index/index.module.scss';
 
 const Index = (props) => (
-  <ul>
-    {props.posts.map((post) => (
-      <li key={post.id}>
-        <Link href={`/posts/${post.slug}`}>
-          <a>{post.title}</a>
-        </Link>
-      </li>
-    ))}
-  </ul>
+  <div>
+    <ul>
+      {props.posts.map((post) => (
+        <li key={post.id}>
+          <Link href={`/posts/${post.slug}`}>
+            <a>{post.title}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </div>
 );
 
 export default Index;
