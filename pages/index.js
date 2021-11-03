6d3c9bdabab40e5255c50df06df144ff2930a860
Seyi Oluwaleimu <s.oluwaleimu@gmail.com>
2021-11-02 23:14:03 -0400

    whole first page is done

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,42 @@
 import styles from 'components/index/index.module.scss';
 import Arrow from '../public/Arrow.svg';
+import { getRecentPost } from 'lib/functions';
+import Post from 'components/Post/Post';
 
-const Index = (props) => (
-  <div className = {styles.homeContainer}>
-    <img src="/FrontImage.png" alt="seyi" className={styles.homeContainer__seyi}/>
-    <div className={styles.homeContainer__homeHeroText}>
-      <h1>HI!</h1>
-      <h1>I'M SEYI</h1>
-      <p>I'm a developer, writer, and student</p>
-      <div className={styles.homeHeroText__CTA}>
-        <Arrow className={styles.bounce}/>
-        <button>Check out my writing!</button>
+
+const Index = ({notFound, post}) => (
+  <>
+    <div className = {styles.homeContainer}>
+      <img src="/FrontImage.png" alt="seyi" className={styles.homeContainer__seyi}/>
+      <div className={styles.homeContainer__homeHeroText}>
+        <h1>HI!</h1>
+        <h1>I'M SEYI</h1>
+        <p>I'm a developer 💻, writer 📜, and student 😁</p>
+        <div className={styles.homeHeroText__CTA}>
+          <Arrow className={styles.bounce}/>
+          <button>Check out my writing!</button>
+        </div>
       </div>
     </div>
-  </div>
+    {
+      !notFound &&
+      <Post data={post}/>
+    }
+  </>
 );
+
+export async function getStaticProps(context) {
+  const post = await getRecentPost();
+
+  if (!post || !post.length) {
+    return {
+      notFound: true,
+    };
+  }
+  return {
+    props: { post: post[0] },
+    revalidate: 1,
+  };
+};
 
 export default Index;
