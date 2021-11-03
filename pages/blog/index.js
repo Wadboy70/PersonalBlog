4b4da564c67a47b1b7b011dcc448ca404dba0ae3
Seyi Oluwaleimu <s.oluwaleimu@gmail.com>
2021-11-02 22:05:44 -0400

    finished home hero

---
+++ pages/blog/index.js
@@ -0,0 +1,34 @@
+import { getPosts } from "../lib/functions";
+import Link from "next/link";
+
+const Blog = ({posts}) => {
+
+    return(                    
+        <ul>
+            {posts.map((post) => (
+            <li key={post.id}>
+                <Link href={`/posts/${post.slug}`}>
+                <a>{post.title}</a>
+                </Link>
+            </li>
+            ))}
+        </ul>
+    );
+};
+
+export async function getStaticProps(context) {
+    const posts = await getPosts();
+  
+    if (!posts) {
+      return {
+        notFound: true,
+      };
+    }
+  
+    return {
+      props: { posts },
+      revalidate: 1,
+    };
+};
+
+export default Blog;
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,19 @@
-import { getPosts } from "../lib/functions";
-import Link from "next/link";
 import styles from 'components/index/index.module.scss';
+import Arrow from '../public/Arrow.svg';
 
 const Index = (props) => (
-  <div>
-    <ul>
-      {props.posts.map((post) => (
-        <li key={post.id}>
-          <Link href={`/posts/${post.slug}`}>
-            <a>{post.title}</a>
-          </Link>
-        </li>
-      ))}
-    </ul>
+  <div className = {styles.homeContainer}>
+    <img src="/FrontImage.png" alt="seyi" className={styles.homeContainer__seyi}/>
+    <div className={styles.homeContainer__homeHeroText}>
+      <h1>HI!</h1>
+      <h1>I'M SEYI</h1>
+      <p>I'm a developer, writer, and student</p>
+      <div className={styles.homeHeroText__CTA}>
+        <Arrow className={styles.bounce}/>
+        <button>Check out my writing!</button>
+      </div>
+    </div>
   </div>
 );
 
 export default Index;
-
-export async function getStaticProps(context) {
-  const posts = await getPosts();
-
-  if (!posts) {
-    return {
-      notFound: true,
-    };
-  }
-
-  return {
-    props: { posts },
-    revalidate: 1,
-  };
-}
