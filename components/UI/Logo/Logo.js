599b1ed4d20fc08ee168bd3a9f1ddfbef2189e0a
Seyi Oluwaleimu <s.oluwaleimu@gmail.com>
2021-11-19 21:04:09 -0500

    css changes

--- components/UI/Logo/Logo.js
+++ components/UI/Logo/Logo.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Image from 'next/image';
 import logo from 'public/logo.png'
 
 import styles from './Logo.module.scss';
@@ -12,12 +11,9 @@ const Logo = () => {
             className={styles.logoContainer}
             href='/'
         >
-            <Image 
-                src={logo} 
+            <img 
+                src="/logo.png" 
                 className={styles.logoContainer_logo}
-                objectFit={"contain"}
-                width={64}
-                height={64}
             />
             <h3 className={styles.logoContainer_name}>Seyi Oluwaleimu</h3>
         </Link>
